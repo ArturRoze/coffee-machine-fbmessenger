26be1277981bf1460f74eee12688547e7ff474f8
ArtyrRoze <a.r.c.h.y@rambler.ru>
2018-01-27 14:47:51 +0200

    webhook

--- src/main/java/app/controller/Webhook.java
+++ src/main/java/app/controller/Webhook.java
@@ -2,12 +2,36 @@ package app.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 public class Webhook {
 
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
+    @RequestMapping("/webhook")
+    public ResponseEntity<String> index(@RequestBody(required = false) String request,
+                                        @RequestParam(name = "hub.verify_token", required = false) String name,
+                                        @RequestParam(name = "hub.challenge", required = false) String challenge) throws IOException {
 
+        System.out.println("request: " + request);
+
+        if (name != null && name.equals("archy198518")) {
+
+            LOGGER.info("validated webhook");
+
+            return new ResponseEntity<>(challenge, HttpStatus.OK);
+
+        } else {
+
+            return ResponseEntity.ok().build();
+        }
+    }
 }
