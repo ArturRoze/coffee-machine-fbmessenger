fd32811bf489019ee25e2520ee8b0c72429e6748
ArtyrRoze <a.r.c.h.y@rambler.ru>
2018-01-27 13:58:50 +0200

    webhook

--- src/main/java/app/controller/Webhook.java
+++ src/main/java/app/controller/Webhook.java
@@ -1,4 +1,13 @@
 package app.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class Webhook {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+
 }
